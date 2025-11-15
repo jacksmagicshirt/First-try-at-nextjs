4dd4e9a4ea04773cc7f4a7d5bb9f6a9004b20ec3
Jacksmagicshirt <flapjack90@gmail.com>
2025-11-15 16:05:19 -0700

    updated github link

--- app/github.js
+++ app/github.js
@@ -2,7 +2,7 @@ export default function GitHub() {
     return(
         <div> 
             <p>my final project is a deck of cards simulator which uses firebase for multiplayer.</p>
-            <p>here is my <a href='https://github.com/jacksmagicshirt/VirtualDeck'> repo!</a></p>
+            <p>here is my <a href='https://github.com/jacksmagicshirt/First-try-at-nextjs'> repo!</a></p>
         </div>
     );
 };
